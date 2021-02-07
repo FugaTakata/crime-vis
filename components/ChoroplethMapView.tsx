db2178758a867da4de475043c18245562f21031d
FugaTakata <maple1213goo@gmail.com>
2021-02-07 14:13:59 +0900

    feature: move 北海道 and 沖縄 and draw lines

--- components/ChoroplethMapView.tsx
+++ components/ChoroplethMapView.tsx
@@ -41,13 +41,13 @@ function ChoroplethMap({ width, height }) {
   const selectedCrime = useRecoilValue(selectedCrimeState);
 
   const side = Math.min(width, height);
-  const scale = 2;
+  const scale = 4;
 
   const projection: GeoProjection = d3
     .geoMercator()
     .scale(side * scale)
     .center([139.69167, 35.68944])
-    .translate([side / 2, side / 2]);
+    .translate([(side * 4) / 5, (side * 3) / 5]);
 
   const path: GeoPath = d3.geoPath().projection(projection);
 
@@ -109,26 +109,87 @@ function ChoroplethMap({ width, height }) {
     .domain([-absMax, 0, absMax])
     .range(["#00f", "#ccc", "#f00"] as Iterable<number>);
 
+  function moveToEndAtIndex(arr, index) {
+    return [
+      ...arr.slice(0, index),
+      ...arr.slice(index + 1),
+      ...arr.slice(index, index + 1),
+    ];
+  }
+
   return (
     <svg width={side} height={side}>
       {features.map((feature, i) => (
-        <path
-          d={path(feature)}
-          stroke="white"
-          fill={
-            rates.filter(
-              (rate) => rate.prefecture === feature.properties.nam_ja
-            )[0].value === null
-              ? "yellow"
-              : (color(
-                  rates.filter(
-                    (rate) => rate.prefecture === feature.properties.nam_ja
-                  )[0].value
-                ) as any)
-          }
+        <g
+          transform={`${
+            feature.properties.nam_ja === "北海道"
+              ? `translate(${(-side / 3) * 2},${side / 3})`
+              : ""
+          }${
+            feature.properties.nam_ja === "沖縄県"
+              ? `translate(${side / 2},${-side / 2.6})`
+              : ""
+          }`}
           key={feature.properties.id}
-        />
+        >
+          <path
+            d={path(feature)}
+            onClick={() => {
+              setSelectedPrefecture(feature.properties.nam_ja);
+              setFeatures((prev) => moveToEndAtIndex(prev, i));
+            }}
+            stroke={
+              selectedPrefecture === feature.properties.nam_ja
+                ? "#363636"
+                : "white"
+            }
+            fill={
+              rates.filter(
+                (rate) => rate.prefecture === feature.properties.nam_ja
+              )[0].value === null
+                ? "yellow"
+                : (color(
+                    rates.filter(
+                      (rate) => rate.prefecture === feature.properties.nam_ja
+                    )[0].value
+                  ) as any)
+            }
+            key={feature.properties.id}
+          />
+        </g>
       ))}
+      <g>
+        <line
+          x1={0}
+          y1={(side / 20) * 9}
+          x2={(side / 5) * 2}
+          y2={(side / 20) * 9}
+          stroke="black"
+        />
+        <line
+          x1={(side / 5) * 2}
+          y1={(side / 20) * 9}
+          x2={(side / 10) * 9}
+          y2={0}
+          stroke="black"
+        />
+      </g>
+      <g>
+        <line
+          x1={side / 3}
+          y1={side}
+          x2={side / 2}
+          y2={(side / 5) * 4}
+          stroke="black"
+        />
+        <line
+          x1={side / 2}
+          y1={(side / 5) * 4}
+          x2={(side / 5) * 3}
+          y2={side}
+          stroke="black"
+        />
+      </g>
     </svg>
   );
 }
