0671672d5b351d86061560f15a8b2e744559b420
FugaTakata <maple1213goo@gmail.com>
2021-02-07 14:17:28 +0900

    change (; ^ ;)

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,22 +24,31 @@ export default function Home() {
       <main className="absolute bg-gray-100 top-20 right-0 bottom-0 left-0">
         <div className="relative w-full h-full">
           {/* box */}
-          <div className="p-3 absolute top-0 right-0 left-0 h-96">
+          <div className="p-3 pb-0 absolute top-0 right-0 left-0 h-1/4">
             <div className="rounded shadow w-full h-full bg-white p-5">
               <div className="w-full h-full">
-                {/* <LineChartView type={"crimes"} /> */}
+                <LineChartView />
               </div>
             </div>
           </div>
           {/* /box */}
           {/* box */}
-          <div className="p-3 absolute top-96 right-0 bottom-0 left-0">
+          <div className="p-3 absolute top-1/4 right-0 bottom-0 left-0">
             <div className="rounded shadow w-full h-full bg-white p-5">
               <div className="w-full h-full">
                 <ChoroplethMapView />
               </div>
             </div>
           </div>
+          {/* /box */}
+          {/* box */}
+          {/* <div className="p-3 pt-0 absolute right-0 bottom-0 left-0 h-1/4">
+            <div className="rounded shadow w-full h-full bg-white p-5">
+              <div className="w-full h-full">
+                <LineChartView />
+              </div>
+            </div>
+          </div> */}
           {/* /box */}
         </div>
       </main>
